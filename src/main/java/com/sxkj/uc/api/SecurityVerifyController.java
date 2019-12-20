4b6781efbf07a7f6249c6f409bccd39549b7b594
zhaoweidong <2911168287@qq.com>
2019-12-20 17:33:46 +0800

    modify code

---
+++ src/main/java/com/sxkj/uc/api/SecurityVerifyController.java
@@ -0,0 +1,10 @@
+package com.sxkj.uc.api;
+
+/**
+ * @ClassName SecurityVerifyController
+ * @Description: TODO
+ * @Author zwd
+ * @Date 2019/12/20 0020
+ **/
+public class SecurityVerifyController {
+}
